3e3a0d46c45ec6d85ab5c7ae06e26d208646c2bf
Göktürk Gök <gokturkgok@gmail.com>
2020-08-26 01:59:44 +0300

    menu-items are updated to navigate withRouter() HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+
+import {Route, Link} from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+
+const HatsPage = () => {
+  return(
+    <div>
+      <h1> HATS PAGE </h1>
+    </div>
+  );
+};
+
+
 function App() {
   return (
-    <div >
-      <HomePage />
+    <div>
+        <Route exact path="/" component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
     </div>
   );
 }
