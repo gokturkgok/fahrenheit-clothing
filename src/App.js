0be375dde3ecca04ac563fb1ed800016286af238
Göktürk Gök <gokturkgok@gmail.com>
2020-09-08 01:31:50 +0300

    Shop page is included along with the preview and collection item component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
 
-import {Route, Link} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
 
-
-const HatsPage = () => {
-  return(
-    <div>
-      <h1> HATS PAGE </h1>
-    </div>
-  );
-};
 
 
 function App() {
   return (
     <div>
         <Route exact path="/" component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
     </div>
   );
 }
