0bddf4ec87eb0c8aecb07ffc42125bf00e2c1660
Göktürk Gök <gokturkgok@gmail.com>
2020-10-02 01:03:59 +0300

    Directory state structure is evolved into Redux.

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,6 +4,7 @@ import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cardReducer from './card/card.reducer';
+import directoryReducer from './directory/directory.reducer';
 
 const persistConfig = {
   key: 'root',
@@ -14,6 +15,7 @@ const persistConfig = {
 const rootReducer = combineReducers({
   user: userReducer,
   card: cardReducer,
+  directory: directoryReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);
