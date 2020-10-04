8dcb4f74567c1551f5622681ce69527fa8771f35
Göktürk Gök <gokturkgok@gmail.com>
2020-10-04 15:25:10 +0300

    Data normalization and collection page is handled & improved.

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,13 +1,5 @@
 import { createSelector } from 'reselect';
 
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    women: 4,
-    mens: 5 
-};
-
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
@@ -15,11 +7,10 @@ export const selectCollections = createSelector(
     shop => shop.collections
 );
 
+
 // collectionUrlParam is a variable of this selector
 export const selectCollection = collectionUrlParam => createSelector(
     [selectCollections],
     collections => 
-        collections.find(
-            collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-        )
+        collections[collectionUrlParam]
 );
