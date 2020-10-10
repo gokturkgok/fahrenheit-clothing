eae44b35b256084ee6cae70b8e92529179c52cc3
Göktürk Gök <gokturkgok@gmail.com>
2020-10-11 02:39:33 +0300

    Shop data is added to Redux.

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -6,14 +6,19 @@ import CollectionPage from '../collection/collection.component';
 
 import { firestore, convertCollectionsSnapShotToMap } from "../../firebase/firebase.utils";
 
+import { connect } from "react-redux";
+import { updateCollections } from '../../redux/shop/shop.actions';
+
 class ShopPage extends React.Component {
     unsubscribeFromSnapshot = null;
 
     componentDidMount() {
+        const { updateCollections } = this.props;
         const collectionRef = firestore.collection('collections');
         
-        collectionRef.onSnapshot(async snapshot => {
-            convertCollectionsSnapShotToMap(snapshot);
+        this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+            const collectionsMap = convertCollectionsSnapShotToMap(snapshot);
+            updateCollections(collectionsMap);
         });
     }
 
@@ -30,4 +35,8 @@ class ShopPage extends React.Component {
 
 };
 
-export default ShopPage;
+const mapDispatchToProps = dispatch => ({
+    updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
